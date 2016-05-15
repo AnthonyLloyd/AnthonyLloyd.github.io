a33edc3f2b516b0d8c858bddc0984e424d55af4e
Anthony Lloyd <anthony_lloyd@yahoo.com>
2016-05-15 23:47:39 +0100

    performance

--- posts/2016-05-20-performance-testing.fsx
+++ posts/2016-05-20-performance-testing.fsx
@@ -9,7 +9,6 @@ keywords: f#, fsharp, functional, higher-order functions, lazy evaluation, modul
 *)
 
 (*** hide ***)
-
 namespace Main
 
 module NumericLiteralG =
@@ -28,20 +27,20 @@ This is the second example of how higher-order functions and lazy evaluation can
 
 ## Background
 
-When performance testing code a number of runs have to be performed so a more stable average can be compared.
-The number of runs is usually an input to the performance testing code and chosen arbitrarily.
+When performance testing code a number of iterations have to be performed so a more stable average can be compared.
+The number of iterations is usually an input to the performance testing code and chosen arbitrarily.
 
 This post will cover how we can use statistical tests to remove the need for this input.
 This simplifies performance testing and makes the results more robust and useful.
 
 ## Statistics
 
-The [standard error](https://en.wikipedia.org/wiki/Standard_error) of the sample mean is given by
+The [standard error](https://en.wikipedia.org/wiki/Standard_error) of a sample mean is given by
 
 $$$
 SE_{\bar{x}} = \frac{s}{\sqrt{n}}
 
-[Welch's t-test](https://en.wikipedia.org/wiki/Welch%27s_t-test) is given by
+[Welch's t-test](https://en.wikipedia.org/wiki/Welch%27s_t-test) for comparing the mean of two samples is given by
 
 $$$
 t = \frac{\bar{x}_1-\bar{x}_2}{\sqrt{f_1+f_2}}
@@ -52,119 +51,148 @@ df = \frac{\left(f_1+f_2\right)^2}{\frac{f_1^2}{n_1-1}+\frac{f_2^2}{n_2-1}}
 $$$
 f_i = \frac{s_i^2}{n_i}
 
-where $n$ is the sample size, $\bar{x}$ is the sample mean and $s^2$ is the sample variance.
+where $n$ is the sample size, $\bar{x}$ is the sample mean, $s^2$ is the sample variance, $t$ is Welch's t statistic, and $df$ is the degrees of freedom of the statistic.
+
+Welch's t statistic can then be compared using the inverse Student's t-distribution for a given confidence interval to test if sample means are different and which one is greater. 
 
 Lazy evaluation can be used to produce an [online](https://en.wikipedia.org/wiki/Online_algorithm) sample statistics sequence.
-This sequence can then be iterated until a given precision or confidence level is satisfied.
+A statistics sequence can then be iterated until a given mean standard error level of accuracy is achieved.
+Two sample statistics sequences can be iterated and compared to decide if one mean is larger than the other.
 
 ## Statistics code
 *)
-
 type SampleStatistics = {N:int;Mean:float;Variance:float}
-                        member x.StandardDeviation = sqrt x.Variance
-                        member x.MeanStandardError = sqrt(x.Variance/float x.N)
-                        static member (*)(f,s) = {s with Mean=s.Mean*f;Variance=s.Variance*f*f}
+                        member s.StandardDeviation = sqrt s.Variance
+                        member s.MeanStandardError = sqrt(s.Variance/float s.N)
 
-type StudentTStatistic = {T:float;DF:int}
+type WelchStatistic = {T:float;DF:int}
 
 module Statistics =
-    let inline sampleStatistics n s =
-        let calc (i,t,t2) =
-            let m = float t/float i
-            let v = (float t2-m*float t)/float(i-1)
-            {N=i*n;Mean=m/float n;Variance=v/float n}
-        Seq.scan (fun (i,t,t2) x -> i+1,t+x,t2+x*x) (0,0G,0G) s |> Seq.skip 3 |> Seq.map calc
+    /// Online statistics sequence for a given sample sequence.
+    let inline sampleStatistics s =
+        let calc (n,t,t2) =
+            let m = float t/float n
+            {N=n;Mean=m;Variance=(float t2-m*float t)/float(n-1)}
+        Seq.scan (fun (n,t,t2) x -> n+1,t+x,t2+x*x) (0,0G,0G) s |> Seq.skip 3 |> Seq.map calc
 
-    let twoSampleTStatistic x1 x2 =
-        let f1 = x1.Variance/float x1.N
-        let f2 = x2.Variance/float x2.N
-        let df = if f1=0.0&&f2=0.0 then 1.0 else sqr(f1+f2)/(f1*f1/float(x1.N-1)+f2*f2/(float(x2.N-1)))
-        {T=(x1.Mean-x2.Mean)/sqrt(f1+f2);DF=int df}
- 
-    //T.INV(0.999,i) ie to 0.1% level
-    let tInv = [|318.309;22.327;10.215;7.173;5.893;5.208;4.785;4.501;4.297;4.144;4.025;3.93;
-                 3.852;3.787;3.733;3.686;3.646;3.61;3.579;3.552;3.527;3.505;3.485;3.467;3.45;
-                 3.435;3.421;3.408;3.396;3.385;3.375;3.365;3.356;3.348;3.34;3.333;3.326;3.319;
-                 3.313;3.307;3.301;3.296;3.291;3.286;3.281;3.277;3.273;3.269;3.265;3.261;3.258;
-                 3.255;3.251;3.248;3.245;3.242;3.239;3.237;3.234;3.232;3.229;3.227;3.225;3.223;
-                 3.22;3.218;3.216;3.214;3.213;3.211;3.209;3.207;3.206;3.204;3.202;3.201;3.199;
-                 3.198;3.197;3.195;3.194;3.193;3.191;3.19;3.189;3.188;3.187;3.185;3.184;3.183;
-                 3.182;3.181;3.18;3.179;3.178;3.177;3.176;3.175;3.175;3.174|]
-    let maxDF = 10000
+    /// Scale the statistics for the given underlying random variable change of scale.
+    let scale f s = {s with Mean=s.Mean*f;Variance=s.Variance*sqr f}
 
-    let twoSampleTTest s1 s2 =
-        let t = twoSampleTStatistic s1 s2
-        if t.DF>maxDF then Some 0
-        elif abs t.T>=Array.get tInv (t.DF-1 |> min (Array.length tInv-1)) then Some(sign t.T)  
-        else None
+    /// Single iteration statistics for the given iteration count and sum statistics.
+    let singleIteration ic s = {N=s.N*ic;Mean=s.Mean/float ic;Variance=s.Variance/float ic}
 
+    /// Student's t-distribution inverse for the 0.1% confidence level by degrees of freedom.
+    let private tInv = 
+        [|636.6;31.60;12.92;8.610;6.869;5.959;5.408;5.041;4.781;4.587;4.437;4.318;4.221;
+          4.140;4.073;4.015;3.965;3.922;3.883;3.850;3.819;3.792;3.768;3.745;3.725;3.707;
+          3.690;3.674;3.659;3.646;3.633;3.622;3.611;3.601;3.591;3.582;3.574;3.566;3.558;
+          3.551;3.544;3.538;3.532;3.526;3.520;3.515;3.510;3.505;3.500;3.496;3.492;3.488;
+          3.484;3.480;3.476;3.473;3.470;3.466;3.463;3.460;3.457;3.454;3.452;3.449;3.447;
+          3.444;3.442;3.439;3.437;3.435;3.433;3.431;3.429;3.427;3.425;3.423;3.421;3.420;
+          3.418;3.416;3.415;3.413;3.412;3.410;3.409;3.407;3.406;3.405;3.403;3.402;3.401;
+          3.399;3.398;3.397;3.396;3.395;3.394;3.393;3.392;3.390|]
+
+    /// Welch's t-test statistic for two given sample statistics.
+    let welchStatistic s1 s2 =
+        let f1 = s1.Variance/float s1.N
+        let f2 = s2.Variance/float s2.N
+        {
+            T = (s1.Mean-s2.Mean)/sqrt(f1+f2)
+            DF= if f1=0.0 && f2=0.0 then 1
+                else sqr(f1+f2)/(sqr f1/float(s1.N-1)+sqr f2/float(s2.N-1)) |> int
+        }
+
+    /// Welch's t-test for a given Welch statistic using a 0.1% confidence level.
+    let welchTest w =
+        if abs w.T < Array.get tInv (min w.DF (Array.length tInv) - 1) then 0 else sign w.T
+(*** hide ***)
+open Statistics
 (**
+## Performance
+
 
 ## Performance code
 *)
-
 module Performance =
-    open Statistics
-
-    let inline private findIterationTarget metric metricTarget f =
-        let rec findN n =
+    let inline private targetIterationCount metric metricTarget f =
+        let rec find n =
             let item = metric n f
             if (item<<<3)>=metricTarget then n*int metricTarget/int item+1
-            else findN (n*10)
-        findN 1
+            else find (n*10)
+        find 1
 
-    let inline private meanMeasure (metric,metricTarget) relativeStandardError f =
-        let n = findIterationTarget metric metricTarget f
-        Seq.initInfinite (fun _ -> metric n f) |> sampleStatistics n
-        |> Seq.where (fun s -> s.MeanStandardError<=relativeStandardError*s.Mean)
-        |> Seq.head
+    let inline private measureStatistics (metric,metricTarget) relativeError f =
+        let ic = targetIterationCount metric metricTarget f
+        Seq.initInfinite (fun _ -> metric ic f) |> sampleStatistics
+        |> Seq.map (singleIteration ic)
+        |> Seq.find (fun s -> s.MeanStandardError<=relativeError*s.Mean)
 
-    let inline private compareMeasure (metric,metricTarget) (f1:unit->'a) (f2:unit->'a) =
+    let inline private measureCompare (metric,metricTarget) f1 f2 =
         if f1()<>f2() then failwith "function results are not the same"
-        let n = findIterationTarget metric metricTarget f2
-        let stats f = Seq.initInfinite (fun _ -> metric n f) |> sampleStatistics n
-        Seq.map2 twoSampleTTest (stats f1) (stats f2) |> Seq.pick id
+        let ic = targetIterationCount metric metricTarget f2
+        let stats f = Seq.initInfinite (fun _ -> metric ic f)
+                      |> sampleStatistics
+                      |> Seq.map (singleIteration ic)
+        Seq.map2 welchStatistic (stats f1) (stats f2)
+        |> Seq.pick (fun w ->
+            let maximumDF = 10000
+            if w.DF>maximumDF then Some 0
+            else
+                let c = welchTest w
+                if c=0 then None else Some c)
 
-    let private measureMetric startMetric endMetric n f =
+    let private measureMetric startMetric endMetric ic f =
         GC.Collect()
         GC.WaitForPendingFinalizers()
         GC.Collect()
         let s = startMetric()
-        let rec loop n = if n>0 then f() |> ignore; loop (n-1)
-        loop n
+        let rec loop i = if i>0 then f() |> ignore; loop (i-1)
+        loop ic
         endMetric s
 
-    let private timeMetric n f = 
-        measureMetric Stopwatch.StartNew (fun sw -> sw.ElapsedTicks) n f
+    let private timeMetric ic f = 
+        measureMetric Stopwatch.StartNew (fun sw -> sw.ElapsedTicks) ic f
 
-    let private memoryMetric n f =
+    let private memoryMetric ic f =
         let inline startMetric() =
-            let succeeded = GC.TryStartNoGCRegion(1L<<<22)
-            if not succeeded then failwith "TryStartNoGCRegion failed"
+            if GC.TryStartNoGCRegion(1L<<<22) |> not then failwith "TryStartNoGCRegion"
             GC.GetTotalMemory false
         let inline endMetric s =
             let t = GC.GetTotalMemory false - s
             GC.EndNoGCRegion()
             t
-        measureMetric startMetric endMetric n f
+        measureMetric startMetric endMetric ic f
 
-    let private garbageMetric n f =
-        let gcCount() = GC.CollectionCount 0 + GC.CollectionCount 1 + GC.CollectionCount 2
-        measureMetric gcCount (fun s -> gcCount() - s) n f
+    let private garbageMetric ic f =
+        let count() = GC.CollectionCount 0 + GC.CollectionCount 1 + GC.CollectionCount 2
+        measureMetric count (fun s -> count() - s) ic f
 
     let private oneMillisecond = Stopwatch.Frequency/1000L
-    let private oneKB = 1024L
     let private timeMeasure = timeMetric, oneMillisecond
-    let private memoryMeasure = memoryMetric, oneKB
+    let private memoryMeasure = memoryMetric, 1024L //1KB
     let private garbageMeasure = garbageMetric, 10
 
-    let private relativeStandardError = 0.01
-
-    let timeStatistics f = meanMeasure timeMeasure relativeStandardError f
-                           |> (*) (1.0/float Stopwatch.Frequency)
-    let memoryStatistics f = meanMeasure memoryMeasure relativeStandardError f
-    let gcCountStatistics f = meanMeasure garbageMeasure relativeStandardError f
+    /// Time statistics for a given function accurate to a mean standard error of 1%.
+    let timeStatistics f =
+        measureStatistics timeMeasure 0.01 f |> scale (1.0/float Stopwatch.Frequency)
+    /// Memory statistics for a given function accurate to a mean standard error of 1%.
+    let memoryStatistics f = measureStatistics memoryMeasure 0.01 f
+    /// GC count statistics for a given function accurate to a mean standard error of 1%.
+    let gcCountStatistics f = measureStatistics garbageMeasure 0.01 f
     
-    let timeCompare f1 f2 = compareMeasure timeMeasure f1 f2
-    let memoryCompare f1 f2 = compareMeasure memoryMeasure f1 f2
-    let gcCountCompare f1 f2 = compareMeasure garbageMeasure f1 f2
+    /// Time comparison for two given functions.
+    let timeCompare f1 f2 = measureCompare timeMeasure f1 f2
+    /// Memory comparison for two given functions.
+    let memoryCompare f1 f2 = measureCompare memoryMeasure f1 f2
+    /// GC count comparison for two given functions.
+    let gcCountCompare f1 f2 = measureCompare garbageMeasure f1 f2
+(**
+## Conclusion
+
+Very simple API
+
+Statistics function give an overview of a function.
+
+Compare can be used in unit tests because it independent of machine.
+
+*)
