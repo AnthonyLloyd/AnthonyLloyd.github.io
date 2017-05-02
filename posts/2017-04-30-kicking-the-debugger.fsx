278e3c907b642cfc6113b093b9d8b9f824cf0c4a
Anthony Lloyd <anthony@lloydhq.com>
2017-05-02 17:58:38 +0100

    example dbg code

--- posts/2017-04-30-kicking-the-debugger.fsx
+++ posts/2017-04-30-kicking-the-debugger.fsx
@@ -9,6 +9,9 @@ keywords: f#, fsharp, debugger, debugging, bugs
 *)
 
 (*** hide ***)
+open System
+open System.Text
+
 module Main
 
 (**
@@ -65,4 +68,81 @@ Finding and fixing bugs feels more like coding and unit testing.
 I can use a simpler code editor plus the command line.
 
 The result is I now have more confidence that once I've created the initial bug test I will be able to resolve it quickly.
+
+### Appendix
+
+I've been asked for some example code from the debug module.
+The code below should hopefully start to give an idea of what can be done:
+*)
+
+#if DEBUG
+[<AutoOpen>]
+module OverflowAndNaNCheck =
+    open Checked
+
+    let inline private isNaN v = match box v with | :? float as v -> Double.IsNaN v | _ -> false
+    let inline (/) a b = let c = a/b in if isNaN c then failwithf "NaN found: %A / %A = %A" a b c else c
+    let inline (+) a b = let c = a+b in if isNaN c then failwithf "NaN found: %A + %A = %A" a b c else c
+    let inline (-) a b = let c = a-b in if isNaN c then failwithf "NaN found: %A - %A = %A" a b c else c
+    let inline (*) a b = let c = a*b in if isNaN c then failwithf "NaN found: %A * %A = %A" a b c else c
+    let inline ( ** ) a b = let c = a**b in if isNaN c then failwithf "NaN found: %A ** %A = %A" a b c else c
+    let inline sqrt a = let c = sqrt a in if isNaN c then failwithf "NaN found: sqrt %A = %A" a c else c
+    let inline log a = let c = log a in if isNaN c then failwithf "NaN found: log %A = %A" a c else c
+    let inline log10 a = let c = log10 a in if isNaN c then failwithf "NaN found: log10 %A = %A" a c else c
+    let inline asin a = let c = asin a in if isNaN c then failwithf "NaN found: asin %A = %A" a c else c
+    let inline acos a = let c = acos a in if isNaN c then failwithf "NaN found: acos %A = %A" a c else c
+    let inline atan a = let c = atan a in if isNaN c then failwithf "NaN found: atan %A = %A" a c else c
+
+module Dbg =
+    let private rand = Random()
+    let mutable private randN = None
+    type atRandom(n:int) =
+        do
+            randN <- Some n
+        interface IDisposable with
+            member __.Dispose() = randN <- None
+    let write fmt =
+        let sb =
+            let n = DateTime.Now
+            let sb = StringBuilder("DEBUG ")
+            sb.Append(n.ToString("dd MMM HH:mm:ss.fffffff")) |> ignore
+            sb.Append("> ") |> ignore
+            sb
+        Printf.kbprintf (fun () ->
+            if Option.isNone randN || Option.get randN |> rand.Next = 0 then
+                let old = Console.ForegroundColor
+                try
+                    Console.ForegroundColor <- ConsoleColor.Red
+                    sb.ToString() |> Console.WriteLine
+                finally
+                    Console.ForegroundColor <- old
+        ) sb fmt
+    let writeIf condition fmt = if condition() then write fmt
+    let runIf condition fn = if condition() then fn()
+    let pipe fmt = fun a -> write fmt a; a
+    let seq desc s =
+        let s = Seq.cache s
+        Seq.iter (write "%s: %A" desc) s
+        s
+    let fun1 desc (f:'a->'b) = fun a -> let b = f a in write "%s - Input: %A\t\t Output: %A" desc a b; b
+    let fun2 desc (f:'a->'b->'c) = fun a b -> let c = f a b in write "%s - Input: %A\t\t Output: %A" desc (a,b) c; c
+    let fun3 desc (f:'a->'b->'c->'d) = fun a b c -> let d = f a b c in write "%s - Input: %A\t\t Output: %A" desc (a,b,c) d; d
+    type counter(desc:string) =
+        let mutable count = 0
+        member __.Count = count
+        member __.Increment() = count <- count + 1
+        member inline m.Calls fn = fun a -> m.Increment(); fn a
+        interface IDisposable with
+            member __.Dispose() = write "%s count = %i" desc count
+    let descendingChecker desc =
+        let mutable last = infinity
+        fun x ->
+            if x>last then write "%s - should be descending but %A > %A" desc x last
+            else last<-x
+    let mutable private functionMap = Map.empty
+    let addFun (key:string) (fn:unit->unit) = functionMap <- Map.add key fn functionMap
+    let runFun (key:string) = Map.find key functionMap ()
+#endif
+
+(**
 *)
