dfb65cf06da493936349817c8f777ceacf2b04fa
Anthony Lloyd <anthony@lloydhq.com>
2017-08-15 13:49:14 +0100

    back to perf

--- posts/2017-08-15-dotnetcore-performance.fsx
+++ posts/2017-08-15-dotnetcore-performance.fsx
@@ -1,20 +1,15 @@
 (**
 \---
 layout: post
-title: ".Net Core 2.0 vs Java Performance Notes"
+title: ".Net Core 2.0 vs Java - Performance Notes"
 tags: [dotnetcore,java,performance,benchmarks]
 description: "Benchmarks Game C# .Net Core 2.0 vs Java Performance Notes"
 keywords: C#, F#, dotnetcore, java, performance, benchmarks
 \---
-*)
 
-(*** hide ***)
-module Main
-    let x = 1
-(**
 Over the past few weeks I've been submitting improvements to some of the C# programs in the [Benchmarks Game](http://benchmarksgame.alioth.debian.org/).
 
-When I first saw the [C# .Net Core vs Java](http://benchmarksgame.alioth.debian.org/u64q/csharp.html) benchmarks the score was **.Net Core 2.0** 4 - **Java** 6.
+When I first saw the [C# .Net Core vs Java](http://benchmarksgame.alioth.debian.org/u64q/csharp.html) benchmarks the score was **.Net Core 2.0 4** - **Java 6**.
 This surprised me as I was under the impression .Net Core 2.0 performance would be very good.
 
 I have concentrated on the 6 programs where Java was faster and not looked at the other 4 (binary-trees, spectral-norm, fasta, pidigits).
@@ -24,40 +19,74 @@ Below are some notes on each of these programs and some caveated conclusions.
 ### reverse-complement
 
 Old Result: C# 1.39s - Java 1.10s  
-Reversing in place and more efficient parallel processing.  
+
+Changes made: Reversing in place and more efficient parallel processing.  
+
 New Result: C# 0.79s - Java 1.10s  
 
 ### mandelbrot
 
 Old Result: C# 7.29s - Java 7.10s  
-Simplified the parallel processing by using TPL Parallel.For.
+
+Changes made: Simplified the parallel processing by using TPL Parallel.For.  
+
 New Result: C# 6.78s - Java 7.10s  
 
 ### n-body
 
 Old Result: C# 21.71s - Java 21.54s  
 
-I made many failed attempts to try to improve this result including using SMD, rem.
+I made many failed attempts to try to improve this result e.g. low level parallel, SMD.
+
+Dissapointing as it looks like Java has a small edge on these single thread numeric calculations.  
+
+Some I submitted can be found [here](https://alioth.debian.org/tracker/index.php?group_id=100815&atid=413122).  
 
 ### fannkuch-redux
 
 Old Result: C# 18.80s - Java 13.74s  
+
+I think Java must have some advantage in small int array manipulation.  
+
+I tried splitting up into more parallel blocks but the overhead outways the better CPU use.  
+
+Change mades: Small array optimisations and more efficient parallel processing.  
+
 New Result: C# 14.98s - Java 13.74s  
 
 ### k-nucleotide
 
 Old Result: C# 13.76s - Java 07.93s  
+
+Java code cheats and has a very bespoke dictionary from an obscure lib.  
+
+The dictionary count is done in parallel but I think the bespoke Java dictionary wins here.  
+
+Change mades: More efficient byte array memory use and parallel processing.   
+
 New Result: C# 12.37s - Java 07.93s  
 
 ### regex-redux
 
 Old Result: C# 32.02s - Java 12.31s  
+
+Regex is not good in .Net. I don't think its even compiled on .Net Core.  
+
+Change mades: Reordered the tasks to run longest to shortest.  
+
 New Result: C# 31.19s - Java 12.31s  
 
 ## Conclusion
 
-Good benchmark process
-Out of ideas
+I found the [Benchmarks Game](http://benchmarksgame.alioth.debian.org/) to be a very good set of benchmarks.
+The benchmarks are well thought through and cover classic single thread calculations to multi threaded IO data processing.
+Obviously areas such as network performance are not as easy to test in this kind of game.
+The orgainisers are tough but fair and the rules make a lot of sense.
+The site is efficiently run and overnight was updated to .Net Core 2.0 RTM.
 
+So now the score is **.Net Core 2.0 6** - **Java 4**.
+My overall impression is that .Net Core 2.0 and Java have similar performance with Java maybe having a small edge on calculations and are manipulation and .Net having better parallel libraries.
+
+So now I am out of ideas on how to improve these further.  If you have any ideas or questions feel to get in touch.
 
 *)
