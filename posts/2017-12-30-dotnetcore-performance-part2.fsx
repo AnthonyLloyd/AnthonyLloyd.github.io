19a150e3f588abae5d413319083bc2c29c2da483
Anthony Lloyd <anthony@lloydhq.com>
2017-12-27 15:51:23 +0000

    con

--- posts/2017-12-30-dotnetcore-performance-part2.fsx
+++ posts/2017-12-30-dotnetcore-performance-part2.fsx
@@ -25,19 +25,23 @@ For `k-nucleotide` the largest dictionary can be constructed more efficiently in
 Another tempting optimisation was in F# there is a one to one replacement to use native pointers for array access e.g. `Array.get a i` becomes `NativePtr.get a i`.
 This actually only provided a small improvement in most cases.
 
+I feel I have to plug [Expect.isFasterThan](https://github.com/haf/expecto#performance-module) in Expecto.
+It has proved to be a quick way of checking that one implementation is truely faster than another.
+
+![isFasterThan](/{{site.baseurl}}public/perf/half-is-faster.png)
+
 ## Results
 
 [C# vs Java](http://benchmarksgame.alioth.debian.org/u64q/csharp.html), [F# vs C#](http://benchmarksgame.alioth.debian.org/u64q/compare.php?lang=fsharpcore&lang2=csharpcore), [F# vs Java](http://benchmarksgame.alioth.debian.org/u64q/compare.php?lang=fsharpcore&lang2=java), [F# vs Haskell](http://benchmarksgame.alioth.debian.org/u64q/compare.php?lang=fsharpcore&lang2=ghc), [F# vs OCaml](http://benchmarksgame.alioth.debian.org/u64q/fsharp.html), [F# vs Python](http://benchmarksgame.alioth.debian.org/u64q/compare.php?lang=fsharpcore&lang2=python3)
 
 | Program            |   C#    |   F#    |  Java   | Haskell |  OCaml  | Python  |
 |:-------------------|--------:|--------:|--------:|--------:|--------:|--------:|
-
 | pidigits           |   **3.03**  |   3.05  |   3.12  |  Error  |  Error  |   3.43  |
 | reverse-complement |   **0.78**  |   0.82  |   1.03  |   1.40  |   0.79  |   3.26  |
-| n-body             |  **21.37**  |  22.86  |  22.10  |  21.43  |  21.67  | 838.39  |
-| mandelbrot         |   **5.83**  |   6.66  |   6.04  |  11.69  |  55.18  | 225.24  |
 | fannkuch-redux     |  **14.44**  |  16.65  |  17.26  |  15.40  |  16.12  | 565.97  |
 | binary-trees       |   **8.26**  |   8.54  |   8.34  |  23.66  |  10.03  |  93.55  |
+| n-body             |  **21.37**  |  22.86  |  22.10  |  21.43  |  21.67  | 838.39  |
+| mandelbrot         |   **5.83**  |   6.66  |   6.04  |  11.69  |  55.18  | 225.24  |
 | fasta              |   2.09  |   **1.67**  |   2.33  |   9.36  |   6.00  |  59.47  |
 | k-nucleotide       |  11.47  |  10.43  |   **8.70**  |  35.01  |  21.63  |  77.65  |
 | regex-redux        |  30.74  |  31.02  |  **10.34**  |  Error  |  24.66  |  15.22  |
@@ -45,16 +49,19 @@ This actually only provided a small improvement in most cases.
 
 ## Conclusion
 
-Usual caveats of current state.
+As said in the previous [post]({% post_url 2017-08-15-dotnetcore-performance %}) there are many caveats to these results.
+They represent the current state of a set of programs on a specific test machine.
+That being said I think there is enough evidence for some general conclusions.
 
-vs C# 15% C# is very good
-vs Java good 5/5 means vs Scala would be v good.
-vs other functional v good.
-.Net Core stunning perf.
+First of all the overall results for .Net Core 2.0 are very impressive compared to other managed languages.
 
-I feel I have to plug [Expect.isFasterThan](https://github.com/haf/expecto#performance-module) in Expecto.
-It has proved to be a quick way of checking that one implementation is truely faster than another.
+F# performance in the worse case is only 15% behind C#. F# is a higher level langauge that results in simpler and shorter code.
+It is good that even in the extreme of a low level performance benchmark it is not too far behind C#.
 
-![isFasterThan](/{{site.baseurl}}public/perf/half-is-faster.png)
+F# in fact shows very good performance against Java resulting in a 5 all draw.
+This means F# would be expected to perform better than Scala or Kotlin if they were to participate in the benchmarks.
+
+F# looks to have the best perfomance across all mainstream functional languages.
+This is due to the efficiency of .Net Core 2.0 and F# being able to be written in a functional-first style.
 
 *)
