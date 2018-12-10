d4cf746c0661bdb336cd44b94ca4c4c547df7c11
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2018-12-10 21:37:06 +0000

    advent day 1

--- posts/2018-12-14-mapslim.fsx
+++ posts/2018-12-14-mapslim.fsx
@@ -1,9 +1,9 @@
 (**
 \---
 layout: post
-title: "MapSlim & SetSlim - From DictionarySlim to Fsion"
+title: "MapSlim - From DictionarySlim to Fsion"
 tags: [mapslim,setslim,listslim,dictionaryslim,fsiom]
-description: "MapSlim & SetSlim - From DictionarySlim to Fsion"
+description: "MapSlim - From DictionarySlim to Fsion"
 keywords: F#, mapslim, setslim, listslim, dictionyslim, fsion
 exclude: true
 \---
@@ -19,10 +19,10 @@ and [Fsion](https://github.com/AnthonyLloyd/Fsion) I realised it was possible to
 
 [DictionarySlim](https://github.com/dotnet/corefxlab/blob/master/src/Microsoft.Experimental.Collections/Microsoft/Collections/Extensions/DictionarySlim.cs)
 is `Dictionary` replacement that can be up to 2.5 times faster for the common `TryGetValue` & `Add` pattern.
-To do this it returns `ref` values and has a more efficient way of using the hash values. It came from ideas used in the
+To do this it returns `ref` values and has a more efficient way of using hash values. It came from ideas used in the
 [Benchmarks Game](https://benchmarksgame-team.pages.debian.net/benchmarksgame/program/knucleotide-csharpcore-9.html).
 
-[Fsion](https://github.com/AnthonyLloyd/Fsion) is a new [WIP](https://dictionary.cambridge.org/dictionary/english/wip) F# bi-temporal database.
+[Fsion](https://github.com/AnthonyLloyd/Fsion) is a new [WIP](https://dictionary.cambridge.org/dictionary/english/wip) bi-temporal database for F#.
 It stores a compressed set of historic values with audit for each entity-attribute.
 The idea is that functional techniques can be used to translate unstructured data to a fully type safe representation.
 
@@ -33,26 +33,28 @@ Each immutable `DataSeries` can be atomically replaced.
 All queries are executed up to a given time/transaction id.
 This simplifies the locking model as entries can be updated at the same time as a number of consistent reads are being made.
 The problem is that a `ReaderWriterLock` would still need to be used on a `Dictionary` for the read and write side.
-By creating lock free for read collections this locking can be simplified to fully lock free for read access.
+By creating lock free for read collections the database can be simplified to fully lock free for read access.
 
-## MapSlim, SetSlim and ListSlim
+## MapSlim at al
 
 Entry updates and collection resize can be made atomic.
 
-[MapSlim](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion/MapSlim.fs)
-[SetSlim](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion/SetSlim.fs)
-[ListSlim]([MapSlim](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion/ListSlim.fs))
-
-Describe the code
+[MapSlim](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion/MapSlim.fs)  
+[SetSlim](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion/SetSlim.fs)  
+[ListSlim]([MapSlim](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion/ListSlim.fs)  
 
 ## Testing
 
 Here's a recent example of the kind of thing you can do in less than 250 lines of code.
 It includes unit tests, property tests, performance tests and threading stress tests.
 
-[MapSlimTests](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion.Tests/MapSlimTests.fs)
-[SetSlimTests](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion.Tests/SetSlimTests.fs)
-[ListSlimTests](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion.Tests/ListSlimTests.fs)
+PIC - stress
+
+[MapSlimTests](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion.Tests/MapSlimTests.fs)  
+[SetSlimTests](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion.Tests/SetSlimTests.fs)  
+[ListSlimTests](https://github.com/AnthonyLloyd/Fsion/blob/master/Fsion.Tests/ListSlimTests.fs)  
+
+PIC - performance
 
 ## Conclusion
 
@@ -60,7 +62,9 @@ Mix of immutable (DataSeries), lock free, and normal collections.
 
 ## References
 
-[Datomic](https://www.datomic.com/)
-[Datomic: Event Sourcing without the hassle](https://vvvvalvalval.github.io/posts/2018-11-12-datomic-event-sourcing-without-the-hassle.html#disqus_thread)
+[Data-First Architecture](http://anthonylloyd.github.io/blog/2018/02/01/architecture-data-first)  
+[Datomic](https://www.datomic.com/)  
+[Datomic: Event Sourcing without the hassle](https://vvvvalvalval.github.io/posts/2018-11-12-datomic-event-sourcing-without-the-hassle.html#disqus_thread)  
 [FASTER](https://github.com/Microsoft/FASTER)
+
 *)
