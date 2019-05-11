13f402868d75e979ab2835bda3b54c8862a1cfea
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-05-11 15:54:52 +0100

    start fsion blogs

---
+++ posts/2019-05-17-fsion-00-introduction.fsx
@@ -0,0 +1,13 @@
+(**
+\---
+layout: post
+title: "Fsion - Introduction"
+tags: [f#,fsion,database,timeseries]
+description: "Fsion - Introduction"
+keywords: F#, fsion, database, timeseries
+exclude: true
+\---
+
+
+
+**)
---
+++ posts/2019-05-17-fsion-01-size.fsx
@@ -0,0 +1,44 @@
+(**
+\---
+layout: post
+title: "Fsion - Size"
+tags: [f#,fsion,database,timeseries]
+description: "Fsion - Size"
+keywords: F#, fsion, database, timeseries
+exclude: true
+\---
+
+### Data details
+
+Tx = 71,262  
+Text : Count = 59,099 Max = 50 Chars = 804,995
+| Count / Bytes | transaction | entitytype | attribute | instrument | position |
+|:-----------|:----------:|:----------:|:----------:|:----------:|:----------:|
+| uri | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 |
+| name | 0 / 0 | 5 / 15 | 20 / 60 | 38,036 / 279,391 | 0 / 0 |
+| time | 71,262 / 909,886 | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 |
+| attribute_type | 0 / 0 | 0 / 0 | 20 / 60 | 0 / 0 | 0 / 0 |
+| attribute_isset | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 |
+| transaction_based_on | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 |
+| isin | 0 / 0 | 0 / 0 | 0 / 0 | 36,476 / 273,162 | 0 / 0 |
+| ticker | 0 / 0 | 0 / 0 | 0 / 0 | 38,036 / 275,050 | 0 / 0 |
+| currency | 0 / 0 | 0 / 0 | 0 / 0 | 38,036 / 240,802 | 0 / 0 |
+| assetclass | 0 / 0 | 0 / 0 | 0 / 0 | 38,023 / 261,032 | 0 / 0 |
+| sector | 0 / 0 | 0 / 0 | 0 / 0 | 37,927 / 258,075 | 0 / 0 |
+| exchange | 0 / 0 | 0 / 0 | 0 / 0 | 12,046 / 79,467 | 0 / 0 |
+| country | 0 / 0 | 0 / 0 | 0 / 0 | 38,036 / 267,549 | 0 / 0 |
+| coupon | 0 / 0 | 0 / 0 | 0 / 0 | 25,552 / 193,815 | 0 / 0 |
+| maturity | 0 / 0 | 0 / 0 | 0 / 0 | 25,546 / 205,849 | 0 / 0 |
+| price | 0 / 0 | 0 / 0 | 0 / 0 | 38,036 / 15,846,249 | 0 / 0 |
+| duration | 0 / 0 | 0 / 0 | 0 / 0 | 25,552 / 5,740,430 | 0 / 0 |
+| fund | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 | 147,323 / 1,184,564 |
+| instrument | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 | 147,323 / 1,096,271 |
+| nominal | 0 / 0 | 0 / 0 | 0 / 0 | 0 / 0 | 147,323 / 8,306,493 |
+
+### Size Estimates
+
+32-bit: Text: 3,028,394 Data: 64,364,789 Total: 67,393,183  
+64-bit: Text: 3,974,002 Data: 78,838,077 Total: 82,812,079  
+Size on disk: 42,348,255
+
+**)
