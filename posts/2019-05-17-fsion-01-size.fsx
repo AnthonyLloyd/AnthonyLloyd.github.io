3fec1a2edae5100e1acf603d9a8a66bb9b52eb3c
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2019-05-13 22:03:04 +0100

    size post

--- posts/2019-05-17-fsion-01-size.fsx
+++ posts/2019-05-17-fsion-01-size.fsx
@@ -8,10 +8,25 @@ keywords: F#, fsion, database, timeseries
 exclude: true
 \---
 
+The sample data set is around 10 months of daily position files for all the
+[iShares](https://www.ishares.com/uk/intermediaries/en/products/etf-product-list#!type=emeaIshares&tab=overview&view=list)
+funds available online.
+All fields in the files are loaded apart from any that can be calculated.
+
+Funds: 280
+Days: 206
+Position files: 71,261
+Size unzipped: 4.4GB
+Size .zip normal: 1.1GB
+Size .7z ultra: 199MB
+
+
+## DataSeries compression
+
+
 ### Data details
 
-Tx = 71,262  
-Text : Count = 59,099 Max = 50 Chars = 804,995
+Text: Count = 59,099 Max length = 50
 
 | Count / Bytes | transaction | entitytype | attribute | instrument | position |
 |:-----------|:----------:|:----------:|:----------:|:----------:|:----------:|
@@ -38,8 +53,27 @@ Text : Count = 59,099 Max = 50 Chars = 804,995
 
 ### Size Estimates
 
-32-bit: Text: 3,028,394 Data: 64,364,789 Total: 67,393,183  
-64-bit: Text: 3,974,002 Data: 78,838,077 Total: 82,812,079  
-Size on disk: 42,348,255
+32-bit: Text = 3,028,394 Data = 64,364,789 Total = 67,393,183  
+64-bit: Text = 3,974,002 Data = 78,838,077 Total = 82,812,079  
+Size on disk = 42,348,255  
+
+<img style="margin-left:20px" src="/{{site.baseurl}}public/fsion/size-by-files.png" title="size by files" />
+
+## Conclusion
+
+This is very handy as the database file size is small enough to commit to github and can be used going forward for testing and performance benchmarking.
+
+Looking at the size on disk compared to 32-bit and 64-bit in memory shows that the objects and pointers contribute quite a lot to the size.
+If the DataSeries were not in a time series compressed format this would be a lot higher.
+This agrees with what I have often found in server caches. Tracking and holding fine grained subsets of the database can actually use a lot of memory.
+
+It also shows my estimates in the [Data-First Architecture](http://anthonylloyd.github.io/blog/2018/02/01/architecture-data-first) blog post are too
+high as it doesn't take account of the DataSeries compression that is possible.
+
+Extrapolating these curves to 10 years of files would give total memory usage of around 650 MB.
+The files contain the key changing attributes.
+A database with a number of additional attributes would be expected to comfortably fit in 1 to 5 GB.
+
+Next we will look at the perfomance characteristics of this data set compared to other options.
 
 *)
