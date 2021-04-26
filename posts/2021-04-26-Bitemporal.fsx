cdee2ea8579cb60c0a04c36cff6cb157f6ad9fcf
Anthony Lloyd <anthony@lloydhq.com>
2021-04-26 17:05:05 +0100

    bitemporal

---
+++ posts/2021-04-26-Bitemporal.fsx
@@ -0,0 +1,12 @@
+(**
+\---
+layout: post
+title: "Bitemporal Source Generator Demo"
+tags: [bitemporal, source generator]
+description: "Bitemporal Source Generator Demo"
+keywords: bitemporal source generator
+\---
+
+See the [demo](https://github.com/AnthonyLloyd/Bitemporal) in github.
+
+*)
