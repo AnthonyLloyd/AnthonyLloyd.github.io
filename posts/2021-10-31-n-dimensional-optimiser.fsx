2ee8383d4f88ee5ee016b477dac06fdff5d72724
Anthony Lloyd <anthony@lloydhq.com>
2021-10-31 12:15:29 +0000

    n-dim opt post

--- posts/2021-10-31-n-dimensional-optimiser.fsx
+++ posts/2021-10-31-n-dimensional-optimiser.fsx
@@ -14,6 +14,7 @@ This has around 50% fewer function calls than Brent across a number of test func
 
 In N dimensions an efficient [BFGS](https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm) algorithm was developed using in place symmetric MKL rank-k, rank-2k functions such that the main loop has no allocations.
 This combined with the above line minimisation results in around 50-70% fewer function calls than other [BFGS](https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm) algorithms across a number of test functions.
+The performance of the algorithm should also scale well with N.
 
 Tolerance parameters across all of the optimisation functions have been made simple, intuative, and consistent.
 
