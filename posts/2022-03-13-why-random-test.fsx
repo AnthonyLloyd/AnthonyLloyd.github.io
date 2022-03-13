20d2eba534bd3e8d664bf184adeac06b8cd30bdf
Anthony Lloyd <anthony@lloydhq.com>
2022-03-13 15:55:59 +0000

    why

--- posts/2022-03-13-why-random-test.fsx
+++ posts/2022-03-13-why-random-test.fsx
@@ -25,7 +25,7 @@ Random tests are able to make a stronger claim than a test with a few examples.
 In fact a test that generates any example and runs for a long time is close to being a proof.
 A 'long time' could be 60 seconds in CsCheck since examples are run in parallel by default and often millions can be run in this time.
 
-When a random test finds a bug it will shrink it down to the smallest possible example so you can more easily repeat and diagnose the problem.
+When a random test finds a bug it will shrink it down to the smallest possible example so you can more easily reproduce and diagnose the problem.
 CsCheck is particularly good at this.
 
 ## Gen It
@@ -37,12 +37,12 @@ The generators created for domain types can be composed and reused across a numb
 We start with a highly defaulted generator `Gen.Double.Array.List` say but may want to be more specific `Gen.Double[0.0, 100.0].Array[5].List[1, 10]` about the range of values. 
 
 Some testing libraries can create the generator for you automatically using reflection but this can lead to a number of bugs for the library author and a lack of control and insight for the library user.
-Fluent style composition similar to LINQ is a much more robust and extendable option.
+Fluent style composition similar to LINQ is a much more robust and extensible option.
 
 ## Some No Brainers
 
 - Serialization - the number of bugs seen in serialization code (looking at you json) is almost criminal given how easy it is to roundtrip test serialization using random testing.
-- Caches and collections - often a key part of server and client side code these can be tested against a suitable simple test model.
+- Caches and collections - often a key part of server and client side code these can be tested against a suitable simplified test model.
 - Calculations and algorithms - often possible to generalize examples for calculations and algorithms and check the result given the input. 
 - Code refactoring - keep a copy of the original code with the test, refactor for simplicity and performance, safe in the knowledge it still produces the same results. Pair with a `Faster` test to monitor the relative performance over a range of inputs.
 - Multithreading and concurrency - test on the same object instance across multiple threads and examples. Shrink even works for concurrency testing.
@@ -52,6 +52,7 @@ Fluent style composition similar to LINQ is a much more robust and extendable op
 CsCheck has been used to help test serialization in Microsoft Orleans and Hagar, and high performance immutable collections in ImTools.
 
 It has given me the ability to write safe native code and interop, high performance collections, faster and simpler server code and much more.
-It basically raised the game in terms of the quality of solutions I can attempt.
+
+I noticed that it increased the quality of solutions I can attempt. It basically raised my game.
 
 *)
