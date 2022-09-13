668ecfe9799afde45c48c57f705d6d990c9845e3
Anthony Lloyd <anthony@lloydhq.com>
2022-09-14 00:17:40 +0100

    allocate

--- posts/2022-09-14-allocate.fsx
+++ posts/2022-09-14-allocate.fsx
@@ -1,13 +1,13 @@
 (**
 \---
 layout: post
-title: "A robust weighted allocation algorithm thanks to CsCheck."
+title: "A robust weighted allocation algorithm thanks to CsCheck"
 tags: [cscheck, random, testing, rounding, allocation, weighted, integer]
-description: "A robust weighted allocation algorithm thanks to CsCheck."
+description: "A robust weighted allocation algorithm thanks to CsCheck"
 keywords: cscheck random testing rounding allocation weighted integer
 \---
 
-Allocating an integer total over a set of weights is a surprisingly tricky problem.
+Allocating an integer total over a set of weights is surprisingly tricky.
 Consider a poll result where round percentages are required to add up to 100.
 Or money or shares to be allocated to a number of accounts.
 
@@ -21,7 +21,7 @@ With the help of [CsCheck](https://www.nuget.org/packages/CsCheck/) we will [rev
 ## CsCheck
 
 We can create random tests to check for these kinds of issues.
-These guide us to the simplest algorithm that satisfy the requirements.
+They guide us to the simplest algorithm that satisfy the requirements.
 The random test code not only gives us many failing simple examples but also hint at the solution.
 
 <img style="border:1px solid black" src="/{{site.baseurl}}public/allocate/tests.png" title="Tests"/>
@@ -38,8 +38,8 @@ Writing random tests leads to a robust algorithm that covers all possible inputs
 
 There were also some nasty precision rounding issues to solve that would be hard to spot using unit testing and a handful of examples.
 
-[Allocate.cs](https://github.com/AnthonyLloyd/CsCheck/blob/master/Tests/Allocator.cs)
-[AllocatorCheck.cs](https://github.com/AnthonyLloyd/CsCheck/blob/master/Tests/AllocatorCheck.cs)
-[AllocatorTests.cs](https://github.com/AnthonyLloyd/CsCheck/blob/master/Tests/AllocatorTests.cs)
+[Allocate.cs](https://github.com/AnthonyLloyd/CsCheck/blob/master/Tests/Allocator.cs)  
+[AllocatorCheck.cs](https://github.com/AnthonyLloyd/CsCheck/blob/master/Tests/AllocatorCheck.cs)  
+[AllocatorTests.cs](https://github.com/AnthonyLloyd/CsCheck/blob/master/Tests/AllocatorTests.cs)  
 
 *)
